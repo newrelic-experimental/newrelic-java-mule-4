0d8e27de5ac0f4c7acee425a31ae23b63578826c
Doug Hilpipre <dhilpipre@users.noreply.github.com>
2022-11-02 16:14:17 -0500

    cleanup from merge

--- Mule-Core-4.4/src/main/java/org/mule/runtime/api/component/execution/CompletableCallback.java
+++ Mule-Core-4.4/src/main/java/org/mule/runtime/api/component/execution/CompletableCallback.java
@@ -3,6 +3,7 @@ package org.mule.runtime.api.component.execution;
 import com.newrelic.api.agent.NewRelic;
 import com.newrelic.api.agent.Trace;
 import com.newrelic.api.agent.weaver.MatchType;
+import com.newrelic.api.agent.weaver.NewField;
 import com.newrelic.api.agent.weaver.Weave;
 import com.newrelic.api.agent.weaver.Weaver;
 import com.newrelic.mule.core.HeaderUtils;
@@ -11,6 +12,7 @@ import com.newrelic.mule.core.NRMuleHeaders;
 @Weave(type=MatchType.Interface)
 public abstract class CompletableCallback<T> {
 	
+	@NewField
 	public NRMuleHeaders headers = null;
 	
 	@Trace
