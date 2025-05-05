e4d0e626f882756ebd60a94d1bd5901e435d80a0
Doug Hilpipre <dhilpipre@newrelic.com>
2025-05-05 14:39:19 -0500

    removed application naming

--- Mule-Extensions/src/main/java/com/nr/instrumentation/mule/extensions/Utils.java
+++ Mule-Extensions/src/main/java/com/nr/instrumentation/mule/extensions/Utils.java
@@ -6,33 +6,11 @@ import java.util.List;
 import org.mule.runtime.api.component.location.ComponentLocation;
 import org.mule.runtime.core.api.source.MessageSource;
 
-import com.newrelic.agent.bridge.AgentBridge;
-import com.newrelic.agent.config.AgentConfig;
-import com.newrelic.api.agent.ApplicationNamePriority;
-import com.newrelic.api.agent.Config;
-import com.newrelic.api.agent.NewRelic;
-
 public class Utils {
 	
 	private static List<String> appNames = new ArrayList<String>();
 	private static Boolean autoNamingEnabled = null;
 	
-	
-	public static void addAppName(String appName) {
-		if(autoNamingEnabled == null) {
-			Config config = NewRelic.getAgent().getConfig();
-			if(config != null && config instanceof AgentConfig) {
-				AgentConfig aConfig = (AgentConfig)config;
-				autoNamingEnabled = aConfig.isAutoAppNamingEnabled();
-			}
-		}
-		if(appName == null || autoNamingEnabled == null || !autoNamingEnabled || appNames.contains(appName)) return;
-		
-		appNames.add(appName);
-		AgentBridge.getAgent().getTransaction(false).setApplicationName(ApplicationNamePriority.REQUEST_ATTRIBUTE, appName);
-		
-	}
-
 	public static String getLocationName(ComponentLocation location) {
 		if(location == null) return null;
 		
